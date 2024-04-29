766262dc1a02a51bd4a19b49daa10bcd715ce502
zerofancy <ntutn.top@gmail.com>
2024-04-30 02:34:33 +0800

    bubble line

--- danmaku-render-engine/src/main/java/com/bytedance/danmaku/render/engine/render/cache/LayerBuffer.kt
+++ danmaku-render-engine/src/main/java/com/bytedance/danmaku/render/engine/render/cache/LayerBuffer.kt
@@ -72,7 +72,7 @@ class LayerBuffer(private val mConfig: DanmakuConfig,
             }
         }
         while (mBufferItems.size > mBufferSize) {
-            val item = mBufferItems.minBy {
+            val item = mBufferItems.minByOrNull {
                 @Suppress("UNCHECKED_CAST")
                 mConfig.common.bufferDiscardRule.invoke(it.data) as Comparable<Any?>
             }
